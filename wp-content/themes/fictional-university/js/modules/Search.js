f7ac8638dc4675f7b60c452b0290eb55fc35a4a2
anatol06 <anatol06@gmail.com>
2018-01-04 22:23:15 +0100

    Implemented custom Rest API

--- wp-content/themes/fictional-university/js/modules/Search.js
+++ wp-content/themes/fictional-university/js/modules/Search.js
@@ -47,7 +47,46 @@ class Search {
 
   getResults() {
 
-    // Asinchronous method (faster)
+    // requests with custom API 
+    var apiUrl = universityData.root_url + '/wp-json/university/v1/search?term=' + this.searchField.val();
+
+    $.getJSON(apiUrl, (results) =>  {
+      this.resultsDiv.html(`
+        <div class="row">
+          <div class="one-third">
+            <h2 class="search-overlay__section-title">General Information</h2>
+            ${results.generalInfo.length ? "<ul class='link-list min-list'>" : "<p>No general information matches that search.</p>"}  
+              ${results.generalInfo.map(item => `<li><a href ="${item.permalink}">${item.title}</a> ${item.postType == 'post' ? ` by ${item.authorName}` : ''}</li>`).join('')}
+            ${results.generalInfo.length ? "</ul>" : ""}
+          </div>
+
+          <div class="one-third">
+            <h2 class="search-overlay__section-title">Programs</h2>
+            ${results.programs.length ? "<ul class='link-list min-list'>" : `<p>No programs match that search. <a href='${universityData.root_url}/programs'>View all programs</a></p>`}  
+              ${results.programs.map(item => `<li><a href ="${item.permalink}">${item.title}</a></li>`).join('')}
+            ${results.programs.length ? "</ul>" : ""}
+
+            <h2 class="search-overlay__section-title">Professors</h2>
+            
+          </div>
+
+          <div class="one-third">            
+            <h2 class="search-overlay__section-title">Campuses</h2>
+            ${results.campuses.length ? "<ul class='link-list min-list'>" : `<p>No campuses match that search. <a href='${universityData.root_url}/campuses'>View all Campuses</a></p>`}  
+              ${results.campuses.map(item => `<li><a href ="${item.permalink}">${item.title}</a></li>`).join('')}
+            ${results.campuses.length ? "</ul>" : ""}
+
+            <h2 class="search-overlay__section-title">Events</h2>
+
+          </div>
+
+        </div>
+      `);
+      this.isSpinnervisible = false;
+    });
+
+  /*  
+    // Asinchronous method (faster) using Wordpress API
     $.when(
       $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val()), 
       $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val())
@@ -64,8 +103,9 @@ class Search {
         }, () => {
           this.resultsDiv.html('<p>Unexpecting eror, please try again.</p>');
     });
+  */
 
-    // Sinchronous method of getting requests
+    // Sinchronous method of getting requests using Wordpress API
    /*$.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
       $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val(), pages => {
         var combinedResults = posts.concat(pages);
--- wp-content/themes/fictional-university/js/scripts-bundled.js
+++ wp-content/themes/fictional-university/js/scripts-bundled.js
@@ -13665,18 +13665,40 @@ function () {
     value: function getResults() {
       var _this = this;
 
-      // Asinchronous method (faster)
-      _jquery.default.when(_jquery.default.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val()), _jquery.default.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val())).then(function (posts, pages) {
-        var combinedResults = posts[0].concat(pages[0]);
+      // requests with custom API 
+      var apiUrl = universityData.root_url + '/wp-json/university/v1/search?term=' + this.searchField.val();
 
-        _this.resultsDiv.html("\n            <h2 class=\"search-overlay__section-title\">General Information</h2> \n              ".concat(combinedResults.length ? "<ul class='link-list min-list'>" : "<p>No general information mathces that search.</p>", "  \n                ").concat(combinedResults.map(function (item) {
-          return "<li><a href =\"".concat(item.link, "\">").concat(item.title.rendered, "</a> ").concat(item.type == 'post' ? " by ".concat(item.authorName) : '', "</li>");
-        }).join(''), "\n              ").concat(combinedResults.length ? "</ul>" : "", "\n          "));
+      _jquery.default.getJSON(apiUrl, function (results) {
+        _this.resultsDiv.html("\n        <div class=\"row\">\n          <div class=\"one-third\">\n            <h2 class=\"search-overlay__section-title\">General Information</h2>\n            ".concat(results.generalInfo.length ? "<ul class='link-list min-list'>" : "<p>No general information matches that search.</p>", "  \n              ").concat(results.generalInfo.map(function (item) {
+          return "<li><a href =\"".concat(item.permalink, "\">").concat(item.title, "</a> ").concat(item.postType == 'post' ? " by ".concat(item.authorName) : '', "</li>");
+        }).join(''), "\n            ").concat(results.generalInfo.length ? "</ul>" : "", "\n          </div>\n\n          <div class=\"one-third\">\n            <h2 class=\"search-overlay__section-title\">Programs</h2>\n            ").concat(results.programs.length ? "<ul class='link-list min-list'>" : "<p>No programs match that search. <a href='".concat(universityData.root_url, "/programs'>View all programs</a></p>"), "  \n              ").concat(results.programs.map(function (item) {
+          return "<li><a href =\"".concat(item.permalink, "\">").concat(item.title, "</a></li>");
+        }).join(''), "\n            ").concat(results.programs.length ? "</ul>" : "", "\n\n            <h2 class=\"search-overlay__section-title\">Professors</h2>\n            \n          </div>\n\n          <div class=\"one-third\">            \n            <h2 class=\"search-overlay__section-title\">Campuses</h2>\n            ").concat(results.campuses.length ? "<ul class='link-list min-list'>" : "<p>No campuses match that search. <a href='".concat(universityData.root_url, "/campuses'>View all Campuses</a></p>"), "  \n              ").concat(results.campuses.map(function (item) {
+          return "<li><a href =\"".concat(item.permalink, "\">").concat(item.title, "</a></li>");
+        }).join(''), "\n            ").concat(results.campuses.length ? "</ul>" : "", "\n\n            <h2 class=\"search-overlay__section-title\">Events</h2>\n\n          </div>\n\n        </div>\n      "));
 
-        _this.isSpinnerVisible = false;
-      }, function () {
-        _this.resultsDiv.html('<p>Unexpecting eror, please try again.</p>');
-      }); // Sinchronous method of getting requests
+        _this.isSpinnervisible = false;
+      });
+      /*  
+        // Asinchronous method (faster) using Wordpress API
+        $.when(
+          $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val()), 
+          $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val())
+          )
+            .then((posts, pages) => {
+              var combinedResults = posts[0].concat(pages[0]);
+              this.resultsDiv.html(`
+                <h2 class="search-overlay__section-title">General Information</h2> 
+                  ${combinedResults.length ? "<ul class='link-list min-list'>" : "<p>No general information mathces that search.</p>"}  
+                    ${combinedResults.map(item => `<li><a href ="${item.link}">${item.title.rendered}</a> ${item.type == 'post' ? ` by ${item.authorName}` : ''}</li>`).join('')}
+                  ${combinedResults.length ? "</ul>" : ""}
+              `);
+              this.isSpinnerVisible = false;
+            }, () => {
+              this.resultsDiv.html('<p>Unexpecting eror, please try again.</p>');
+        });
+      */
+      // Sinchronous method of getting requests using Wordpress API
 
       /*$.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
          $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val(), pages => {
